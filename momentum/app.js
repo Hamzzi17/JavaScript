ffbb0fe067f4d333998f9c4c808e7a2a7827eee7
희선 <101498678+Hamzzi17@users.noreply.github.com>
2023-04-19 19:06:15 +0900

    230415~

--- momentum/app.js
+++ momentum/app.js
@@ -40,13 +40,75 @@
 
 // const player = ["heesun", 121212, true, "little bit"];
 
-const player = {
-    name: "heesun",
-    points: 10,
-    fat: false,
+// const player = {
+//     name: "heesun",
+//     points: 10,
+//     fat: false,
+// };
+// console.log(player);
+// player.fat = true; // We can't modify a constant, but I have no problem if I want update something(object) 'inside' of constant.
+// player.lastName = "potato";
+// player.points = player.points + 15; // 25
+// console.log(player.points);
+
+// function sayHello(nameOfPerson, age) {
+//     console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
+
+// sayHello("heesun", 25);
+// sayHello("dal", 23);
+// sayHello("lynn", 21);
+
+// // "Hello my name is " + myName;
+
+// function plus(firstNumber, secondNumber) {
+//     console.log(firstNumber + secondNumber);
+// }
+// function divide(a, b) {
+//     console.log(a / b);
+// }
+// plus(60, 8);
+// divide(98, 20);
+
+// player.sayHello()
+
+// function sayHello(){
+
+// }
+
+// const player = {
+//     name: "heesun",
+//     sayHello: function(otherPersonsName) {
+//     console.log("hello " + otherPersonsName + " nice to meet you!");
+//     },
+// };
+
+// console.log(player.name);
+// player.sayHello("lynn");
+// player.sayHello("heesun");
+
+// 계산기 만들기
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    powerOf: function (a, b) {
+        console.log(a ** b);
+    }
 };
-console.log(player);
-player.fat = true; // We can't modify a constant, but I have no problem if I want update something(object) 'inside' of constant.
-player.lastName = "potato";
-player.points = player.points + 15; // 25
-console.log(player.points);
+
+calculator.add(5, 1);
+calculator.minus(7, 2);
+calculator.divide(21, 3);
+calculator.powerOf(3, 2);
+// 1+1, 1-1, a+b, a-b, a/b, a**b 구현
+// calculator.add(1, 2)
+// calculator.minus(1, 2)
+// calculator.divide(1, 2)
+// calculator.powerOf(1, 2)
