79ccc1c869f45d50cfaf5a61d6c3b83b12d9ca17
희선 <101498678+Hamzzi17@users.noreply.github.com>
2023-04-19 16:17:26 +0900

    230415,16,17

---
+++ momentum/app.js
@@ -0,0 +1,9 @@
+const player = {
+    name: "heesun",
+    points: 10,
+    fat: false,
+};
+console.log(player);
+player.lastName = "potato";
+player.points = player.points + 15;
+console.log(player.points);
